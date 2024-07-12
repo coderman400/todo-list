8e6f886661ddfd419814b2e68a07508362f54cd9
arvind <achurox1121@gmail.com>
2024-07-12 16:14:20 +0530

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("hi");
+function Todo(title,description,dueDate,priority){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+
+    const info = ()=>{
+        return `${this.title} : ${this.description} due by ${this.dueDate}, ${priority} priority`
+    }
+
+    const toProject = (project) =>{
+        project.add(this);
+    }
+
+    defaultProject.add(this);
+
+    return {info, toProject}
+}
+
+function Project(title){
+    this.title = title;
+    let list = [];
+
+    const add = (todo) =>{
+        list.push(todo);
+    }
+
+
+    return {add, list}
+}
+
+defaultProject= new Project("default");
+t1 = new Todo("clean", "u know", "tomorrow", "high") 
