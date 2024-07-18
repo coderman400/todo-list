2c474e6ec371b0bfc5ffbb2b4d2e36b7988c592f
arvind <achurox1121@gmail.com>
2024-07-18 22:11:20 +0530

    add logo, style changes

--- src/display.js
+++ src/display.js
@@ -1,4 +1,5 @@
 import appDriver from './index.js';
+import cheyyImg from './cheyy.png';
 
 let a = new appDriver();
 
@@ -6,3 +7,9 @@ let t1= a.createTask("clean", "u know", "tomorrow", "high")
 let t2= a.createTask("wipe", "wat", "hehe", "meow")
 
 let content = document.getElementById("content");
+let nav = document.getElementById("navbar");
+let cheyy = new Image();
+cheyy.src= cheyyImg;
+cheyy.id = 'cheyy';
+nav.prepend(cheyy);
+
